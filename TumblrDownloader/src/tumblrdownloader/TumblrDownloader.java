c5a8514f8dca2945d01b849087b26f652171d563
vmunozre <v.munozre@alumnos.urjc.es>
2016-03-28 16:50:09 +0200

    Cambiado la forma de recoger el Tumblr. Ahora se recoge el id del usuario, no el enlace entero.

--- TumblrDownloader/src/tumblrdownloader/TumblrDownloader.java
+++ TumblrDownloader/src/tumblrdownloader/TumblrDownloader.java
@@ -12,13 +12,17 @@ public class TumblrDownloader {
     public static void main(String[] args) {
         //Ejemplo enlace sencillo: http://pruebasphp.victorreiner.com/pruebas/pruebaarticulos.html
         //ejemplo tumblr : http://fantasticpapersong.tumblr.com/
-        System.out.println("Introduzca la URL del tumblr: ");
-        System.out.println("*Ejemplo: http://fantasticpapersong.tumblr.com/*");
+        System.out.println("Introduzca la id del tumblr: ");
+        System.out.println("*Ejemplo: 'fantasticpapersong' http:// | fantasticpapersong | .tumblr.com/*");
         Scanner in = new Scanner(System.in);
-        String enlace = in.next();
-        String directorio = enlace.substring(7,enlace.indexOf("."));
-
-        TumblrProcessor tumblr = new TumblrProcessor(("./" + directorio), 10, 5);
+        String id = in.next();
+        
+        String enlace = ("http://" + id + ".tumblr.com/");
+        //Ahora sabemos, si o si, que el enlace empieza por http:// y acaba por .tumblr.com/
+        System.out.println(enlace);
+        //String directorio = enlace.substring(7,enlace.indexOf("."));
+        
+        TumblrProcessor tumblr = new TumblrProcessor(("./downloads/" + id), 10, 5);
 
         System.out.println("Procediendo a decargar imagenes:");
         tumblr.process(enlace);
